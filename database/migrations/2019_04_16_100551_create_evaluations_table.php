34fde123c3a9579300ef46a955cc19cda4b62f18
Alice Carry <alice.carry@lilo.org>
2019-04-16 12:10:43 +0200

    [BDD] Ajout de la migration de la table evaluations

---
+++ database/migrations/2019_04_16_100551_create_evaluations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEvaluationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('evaluations', function (Blueprint $table) {
+            $table->integer('id')->autoIncrement();
+            $table->string('question', 255);
+            $table->string('answer', 255);
+            $table->integer('chapters_id');
+            $table->foreign('chapters_id')->references('id')->on('chapters');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('evaluations');
+    }
+}
