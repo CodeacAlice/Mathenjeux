b9d09bdbc32779872ef261ebd6520fc94796ab46
Alice Carry <alice.carry@lilo.org>
2019-04-24 13:57:20 +0200

    [Bug fixed] Amélioration du code pour changer la taille du support, accepte la présence ou l'absence de toute div prévue

--- public/js/responsive.js
+++ public/js/responsive.js
@@ -21,17 +21,22 @@ window.onclick = function(event) {
 // Adapter la hauteur du support à la hauteur de l'écran
 var newheights; var newheightc; var supp;
 var bod; var ent; var nav; var foot; var err;
+var allheights = [];
 
 function sizesup() {
 	bod = $('body').height();
-	ent = $('#entete').height();
-	nav = $('nav').height() + 2*parseInt($('nav').css('padding-top'));
-	foot = $('footer').height() + parseInt($('footer').css('padding-top')) + parseInt($('footer').css('padding-bottom'));
-	err = $('#error').height() + 2*parseInt($('#error').css('padding-top'));
+	ent = $('#entete').height() + parseInt($('#entete').css('padding-top')) + parseInt($('#entete').css('padding-bottom')) + parseInt($('#entete').css('margin-top')) + parseInt($('#entete').css('margin-bottom'));
+	nav = $('nav').height() + parseInt($('nav').css('padding-top')) + parseInt($('nav').css('padding-bottom')) + parseInt($('nav').css('margin-top')) + parseInt($('nav').css('margin-bottom'));
+	foot = $('footer').height() + parseInt($('footer').css('padding-top')) + parseInt($('footer').css('padding-bottom')) + parseInt($('footer').css('margin-top')) + parseInt($('footer').css('margin-bottom'));
+	err = $('#error').height() + parseInt($('#error').css('padding-top')) + parseInt($('#error').css('padding-bottom')) + parseInt($('#error').css('margin-top')) + parseInt($('#error').css('margin-bottom'));
+
+	allheights = [ent, nav, foot, err];
 
 	supp = $('#support').height() + parseInt($('#support').css('padding-top')) + parseInt($('#support').css('padding-bottom'));;
-	newheights = bod - ent - nav - foot;
-	if (err > 0) {newheights -= err;}
+	newheights = bod;
+	for (var i = 0; i < allheights.length; i++) {
+		if (allheights[i] > 0) {newheights -= allheights[i]}
+	}
 
 	if (supp < newheights) {
 		$('#support').css('min-height', (newheights)+'px');
